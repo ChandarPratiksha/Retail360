7eced072a1745f52a575d08b177df7d8694685f4
Pratiksha Balasaheb Chandar <101510109+ChandarPratiksha@users.noreply.github.com>
2026-03-28 12:14:40 +0530

    Use environment variables for Supabase credentials

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-
-const SUPABASE_URL = "https://oopqdrvtjaovpwhrdhax.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vcHFkcnZ0amFvdnB3aHJkaGF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkyMTIwMDMsImV4cCI6MjA4NDc4ODAwM30.y8Ni-MOg1qMt-vTKkEnxh0p6JRASZtbPimRCsOTSz5s";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_KEY;
 
 window.supabaseClient = supabase.createClient(
   SUPABASE_URL,
